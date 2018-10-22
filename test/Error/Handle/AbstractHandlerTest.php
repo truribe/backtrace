911aa51220622fe4403ece3477e40d0da33c2f94
Travis Uribe <travis@turibe.com>
2018-10-21 22:38:42 -0700

    fixup! 532e998bfa2232e5b1ff872f8bca77bbc4f7c890

--- test/Error/Handle/AbstractHandlerTest.php
+++ test/Error/Handle/AbstractHandlerTest.php
@@ -14,7 +14,7 @@ use tvanc\backtrace\Error\Handle\ErrorHandlerInterface;
  * Just a convenience class for handler test cases to extend if they need the
  * same handler multiple times.
  */
-abstract class AbstractHandlerTest extends TestCase
+abstract class AbstractHandlerTest extends TestCase implements HandlerTestInterface
 {
     abstract public function getHandler(): ErrorHandlerInterface;
 }
