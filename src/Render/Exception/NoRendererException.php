51c579a07d04eb6919c9ce8d6d9c7155cb2405ce
Travis Uribe <travis@turibe.com>
2018-10-31 19:26:50 -0700

    Add exception class for when no renderer is defined

---
+++ src/Render/Exception/NoRendererException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * TODO Add @file block for NoRendererException.php
+ *
+ * @author Travis Uribe <travis@tvanc.com>
+ */
+
+namespace tvanc\backtrace\Render\Exception;
+
+/**
+ * TODO Document class NoRendererException
+ */
+class NoRendererException extends \Exception
+{
+}
