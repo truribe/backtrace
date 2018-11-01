b65c56576839f26f0d108acbc9da5b66e683d05d
Travis Uribe <travis@turibe.com>
2018-10-31 19:20:13 -0700

    Add ShutdownException class

---
+++ src/Error/Listen/Exception/ShutdownException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * TODO Add @file block for ShutdownException.php
+ *
+ * @author Travis Uribe <travis@tvanc.com>
+ */
+
+namespace tvanc\backtrace\Error\Listen\Exception;
+
+/**
+ * TODO Document class ShutdownException
+ */
+class ShutdownException extends \ErrorException
+{
+}
