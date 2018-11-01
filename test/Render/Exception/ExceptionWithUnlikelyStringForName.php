bced4db2ae405fd9cfe9c390bb7549e463793072
Travis Uribe <travis@turibe.com>
2018-10-31 19:29:08 -0700

    Add special exception with crazy name to make it easy to find in render output

---
+++ test/Render/Exception/ExceptionWithUnlikelyStringForName.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Travis Uribe <travis@tvanc.com>
+ */
+
+namespace tvanc\backtrace\Test\Render\Exception;
+
+/**
+ * This class exists to aid in verifying that implementations of
+ * ExceptionRendererInterface render the correct information.
+ */
+class ExceptionWithUnlikelyStringForName extends \Exception
+{
+}
