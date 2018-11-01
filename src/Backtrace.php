2b0f9ca823ca3aa0ea3bed1c648f516e9d1302ed
Travis Uribe <travis@turibe.com>
2018-10-31 19:32:16 -0700

    Improve still-ugly utility method getErrorType()

--- src/Backtrace.php
+++ src/Backtrace.php
@@ -29,45 +29,49 @@ final class Backtrace
     /**
      * TODO Refactor this to be pretty.
      *
-     * @param \Throwable $ex
+     * @param \Throwable $throwable
      * @param bool       $shorten
      *
      * @return string
      */
-    static function getErrorType($ex, $shorten = true)
+    static function getErrorType($throwable, $shorten = true)
     {
-        if ($ex instanceof \ErrorException) {
-            $severity = $ex->getSeverity();
+        if ($throwable instanceof \ErrorException) {
+            $severity = $throwable->getSeverity();
 
             $typeNameMap = [
-                E_ERROR => 'Fatal error',
-                E_PARSE => 'Parse error',
-                E_CORE_ERROR => 'Core error',
-                E_COMPILE_ERROR => 'Compile error',
-                E_USER_ERROR => 'User error',
-                E_NOTICE => 'Notice',
-                E_DEPRECATED => 'Deprecated',
-                E_USER_NOTICE => 'User notice',
-                E_WARNING => 'Warning',
-                E_USER_WARNING => 'User warning',
-                E_CORE_WARNING => 'Core warning',
-                E_COMPILE_WARNING => 'Compile warning',
-                E_STRICT => 'Strict message',
+                E_ERROR             => 'Fatal error',
+                E_PARSE             => 'Parse error',
+                E_CORE_ERROR        => 'Core error',
+                E_COMPILE_ERROR     => 'Compile error',
+                E_USER_ERROR        => 'User error',
+                E_NOTICE            => 'Notice',
+                E_DEPRECATED        => 'Deprecated',
+                E_USER_NOTICE       => 'User notice',
+                E_WARNING           => 'Warning',
+                E_USER_WARNING      => 'User warning',
+                E_CORE_WARNING      => 'Core warning',
+                E_COMPILE_WARNING   => 'Compile warning',
+                E_STRICT            => 'Strict message',
                 E_RECOVERABLE_ERROR => 'Recoverable error',
-                E_USER_DEPRECATED => 'User deprecated',
+                E_USER_DEPRECATED   => 'Deprecated',
             ];
 
             return $typeNameMap[$severity];
         }
 
-        $className = get_class($ex);
-        $slashPos = strrpos($className, '\\');
+        if ($shorten) {
+            try {
+                $reflection = new \ReflectionClass($throwable);
 
-        if ($shorten && $slashPos !== false) {
-            return substr($className, $slashPos + 1);
+                return $reflection->getShortName();
+            }
+            catch (\Exception $exception) {
+                // If exception is thrown just return the plain ol' FQCN
+            }
         }
 
-        return $className;
+        return get_class($throwable);
     }
 
 
