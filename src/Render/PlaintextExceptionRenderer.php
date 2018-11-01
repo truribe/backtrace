26027a4740da8f66e1fc17888dd17aba281058c0
Travis Uribe <travis@turibe.com>
2018-10-31 19:28:32 -0700

    Add skeleton PlaintextExceptionRenderer class

---
+++ src/Render/PlaintextExceptionRenderer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * TODO Add @file block for PlaintextBacktraceRenderer.php
+ *
+ * @author Travis Uribe <travis@tvanc.com>
+ */
+
+namespace tvanc\backtrace\Render;
+
+/**
+ * TODO Document class PlaintextBacktraceRenderer
+ */
+class PlaintextExceptionRenderer extends AbstractExceptionRenderer
+{
+    public function render(\Throwable $throwable): string
+    {
+        // TODO: Implement render() method.
+    }
+
+
+    public function renderStage(array $stage): string
+    {
+        // TODO: Implement renderStage() method.
+    }
+}
