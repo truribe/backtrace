cec26d5f3beb636fb770abc11f0a4e54e72d1473
Travis Uribe <travis@turibe.com>
2018-11-02 20:42:57 -0700

    Remove tests of PHP features
    I thought I was testing my code... but really I'm just testing a feature of PHP. Unfortunately, it doesn't work - at least not how I _expected_ it to. I may have misread.

--- test/Error/Listener/ErrorListenerTest.php
+++ test/Error/Listener/ErrorListenerTest.php
@@ -5,11 +5,9 @@
 
 namespace tvanc\backtrace\Test\Error\Listener;
 
-use PHPUnit\Framework\Error\Warning;
 use PHPUnit\Framework\TestCase;
 use tvanc\backtrace\Error\Listener\ErrorListener;
 use tvanc\backtrace\Error\Listener\Exception\NoResponderException;
-use tvanc\backtrace\Error\Responder\ErrorResponderInterface;
 
 /**
  * Tests ErrorListener
@@ -34,59 +32,5 @@ class ErrorListenerTest extends TestCase
         $listener->catchThrowable(new \Exception(
             'Thrown an Exception; landed an UnhandledExceptionException'
         ));
-    }
-
-
-    /**
-     * Verify errors are passed back to PHP when override is disabled.
-     *
-     * Tested separately from the effects of enabling override to sidestep
-     * awkwardness of testing a single output buffer for mutually contradictory
-     * results.
-     *
-     * @see testOverrideOn()
-     */
-    public function testOverrideOff()
-    {
-        // Create listener with override DISABLED and noop responder
-        $listener = new ErrorListener([
-            $this->createMock(ErrorResponderInterface::class)
-        ], false);
-
-        // Register the listener as an error handler
-        $listener->listenForErrors();
-
-        $message = 'This error SHOULD go to native error handler.';
-
-        $this->expectException(Warning::class);
-        $this->expectExceptionMessage($message);
-
-        trigger_error($message, E_USER_WARNING);
-    }
-
-
-    /**
-     * Verify that errors are NOT passed back to PHP when override is enabled.
-     *
-     * Tested separately from the effects of disabling override to sidestep
-     * awkwardness of testing a single output buffer for mutually contradictory
-     * results.
-     *
-     * @see testOverrideOff()
-     * @see ErrorListener::override
-     */
-    public function testOverrideOn()
-    {
-        // Create listener with override ENABLED and noop responder
-        $listener = new ErrorListener([
-            $this->createMock(ErrorResponderInterface::class)
-        ], true);
-
-        // Register the listener as an error handler
-        $listener->listenForErrors();
-
-        $message = 'This error should NOT go to native error handler.';
-        $this->expectOutputString('');
-        trigger_error($message, E_USER_WARNING);
     }
 }
