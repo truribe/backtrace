95b75d6d847a91e3341850da1059152826447ecf
Travis Uribe <travis@turibe.com>
2018-11-03 20:55:41 -0700

    Add coverage for PlaintextExceptionRenderer

---
+++ test/Render/PlaintextExceptionRendererTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * TODO Add @file block for PlaintextExceptionRendererTest.php
+ *
+ * @author Travis Van Couvering <travis@tvanc.com>
+ */
+
+namespace tvanc\backtrace\Test\Render;
+
+use tvanc\backtrace\Render\ExceptionRendererInterface;
+use tvanc\backtrace\Render\PlaintextExceptionRenderer;
+
+/**
+ * Test formatting of plaintext exception rendering
+ */
+class PlaintextExceptionRendererTest extends AbstractExceptionRendererTest
+{
+    /**
+     * Get the renderer implementation for this suite of tests.
+     *
+     * @return ExceptionRendererInterface
+     */
+    public function getRenderer(): ExceptionRendererInterface
+    {
+        return new PlaintextExceptionRenderer();
+    }
+}
